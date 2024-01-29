cf6b907e26fa1dd4d5addfb67d7255662b0af66f
LOISGALLAUD <loisgallaud@hotmail.com>
2024-01-29 17:51:05 +0100

    scraping all the pokemon

--- colly-test/main.go
+++ colly-test/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 	"sync"
 
 	"github.com/gocolly/colly"
 )
 
+const (
+	SITE = "https://scrapeme.live/shop/page"
+	PRODUCT = "li.product"
+	PRODUCT_URL = "a"
+	PRODUCT_IMAGE = "img"
+	PRODUCT_NAME = "h2"
+	PRODUCT_PRICE = ".price"
+	MAX_PAGE = 48
+)
+
 type PokemonProduct struct {
 	url, image, name, price string
 }
-
 
 func main() {
 	log.Println("Program started.")
@@ -19,44 +30,40 @@ func main() {
 	// Initialize
 	var products []PokemonProduct
 	var wg sync.WaitGroup
-	const site string = "https://scrapeme.live/shop/"
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	err = writer.Write([]string{"url", "image", "name", "price"})
+	if err != nil {
+		log.Fatal("Cannot write to file", err)
+	}
 
 	c := colly.NewCollector()
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-	
 	c.OnError(func(_ *colly.Response, err error) {
 		log.Println("Something went wrong:", err)
 	})
-	
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Visited", r.Request.URL)
-	})
-	
-	c.OnHTML("li.product", func(e *colly.HTMLElement) {
+	c.OnHTML(PRODUCT, func(e *colly.HTMLElement) {
 		pokemonProduct := PokemonProduct{}
-		pokemonProduct.url = e.ChildAttr("a", "href")
-		pokemonProduct.image = e.ChildAttr("img", "src")
-		pokemonProduct.name = e.ChildText("h2")
-		pokemonProduct.price = e.ChildText(".price")
+		pokemonProduct.url = e.ChildAttr(PRODUCT_URL, "href")
+		pokemonProduct.image = e.ChildAttr(PRODUCT_IMAGE, "src")
+		pokemonProduct.name = e.ChildText(PRODUCT_NAME)
+		pokemonProduct.price = e.ChildText(PRODUCT_PRICE)
 		products = append(products, pokemonProduct)
 	})
-	
-	c.OnScraped(func(r *colly.Response) {
-		log.Println("Finished", r.Request.URL)
-	})
-
 	log.Println("Initialized successfully.")
 
-
-	// Start scraping on the sites
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		c.Visit(site)
-	}()
+	// Start scraping on the different pages
+	wg.Add(MAX_PAGE)
+	for i := 1; i <= MAX_PAGE; i++ {
+		go func(i int) {
+			defer wg.Done()
+			c.Visit(fmt.Sprintf("%s/%d/", SITE, i))
+		}(i)
+	}
 	log.Println("Scraping started successfully, please wait...")
 
 	// Collect data from the sites
@@ -65,8 +72,14 @@ func main() {
 
 	// Print the data
 	for _, product := range products {
-		fmt.Printf("URL: %s\nImage: %s\nName: %s\nPrice: %s\n\n",
-		 product.url, product.image, product.name, product.price)
+		err := writer.Write([]string{
+		product.url,
+		product.image,
+		product.name,
+		product.price})
+		if err != nil {
+			log.Fatal("Cannot write to file", err)
+		}
 	}
 
 	log.Println("Program terminated.")
